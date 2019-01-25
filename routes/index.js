c843f87e516670aa793cded50a43bcc1606bc4a2
Zac Crosby <Zacros7164@gmail.com>
2019-01-25 17:03:26 -0500

    added beginning of user login systems

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+
+const mysql = require('mysql');
+const config = require('../config');
+const connection = mysql.createConnection(config.db);
+connection.connect()
+
 const request = require('request');
 // our node module in .gitignore
 // ../config is now a node module, a really tiny one, but still a module
-const apiKey = require('../config');
 const apiBaseUrl = 'http://api.themoviedb.org/3';
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
-const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${config.apiKey}`;
 
 
 
@@ -39,7 +44,30 @@ router.post('/search/movie',(req,res)=>{
   // submitted data from forms comes in the req Object
   // queryString data is in req.query
   // posted data is in req.body
+  const movieTitle = req.body.movieTitle;
+  const searchUrl = `${apiBaseUrl}/search/movie?query=${movieTitle}&api_key=${config.apiKey}`
+  request.get(searchUrl, (error,response,body)=>{
+    const parsedData = JSON.parse(body);
+    res.render('now_playing', {
+      parsedData: parsedData.results,
+      title: "Search Results",
+      imageBaseUrl,
+    })
+  })
+  
+})
 
+
+router.get('/login', (req,res)=>{
+  res.render('login')
+})
+
+router.post('/loginProcess',(req,res)=>{
+
+  const insertQuery = `INSERT into users ( email, password)
+    VALUES
+    (?,?);`;
+  
   res.json(req.body)
 })
 
